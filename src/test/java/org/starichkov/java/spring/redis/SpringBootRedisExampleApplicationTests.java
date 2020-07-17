3af1a6cdaa544f579c0eb1387f9e05c9de2ceaa1
Vadim Starichkov <starichkovva@gmail.com>
2020-07-17 17:38:07 +0300

    Initial commit

---
+++ src/test/java/org/starichkov/java/spring/redis/SpringBootRedisExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.starichkov.java.spring.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedisExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
